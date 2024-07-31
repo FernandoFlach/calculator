440c199e5d46ba1c64a402047abd238817a573d7
Fernando Flach <fernandoflach88@gmail.com>
2024-07-31 19:09:44 -0300

    Add functions to alter the display when buttons is clicked.
    Also added a few operation functions to be used later.

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
 
-// const button = document.querySelector(".buttonTest");
-// button.addEventListener("click", () => alert("buttonclicked"));
+let firstNumber, operator, secondNumber;
 
-const buttons = [...document.querySelectorAll(".button")];
 
-const signs = [...document.querySelectorAll(".button")].filter((id) => id.id.includes("Sign"));
+const display = document.querySelector("#display")
 
+const numbers = [...document.querySelectorAll(".button")].filter((id) => id.id.includes("b"));  // Includes the operation signs
+const signs = [...document.querySelectorAll(".button")].filter((id) => id.id.includes("Sign")); // Clear, backspace, dot, equal sign
+
+for (i in numbers){
+    let number = numbers[i].id[1];
+    numbers[i].addEventListener("click", () => {
+        alterScreen(number);
+    })
+}
+
+
+
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a,b){
+    return a - b;
+};
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+};
+
+
+function operate(a,b,op){
+    if (op == "+"){
+        add(a,b);
+    } 
+    else if (op == "-"){
+        subtract(a,b)
+    }
+    else if (op == "/"){
+        divide(a,b);
+    }
+    else if (op == "x"){
+        multiply(a,b);
+    }
+}
+
+function alterScreen(button){
+
+    if (isNaN(button)){  
+        display.textContent = button;  // For operation signs
+    }
+    else if (!isNaN(display.textContent)){  // When number has more than one digit
+        display.textContent += button;
+    }
+    else {
+        display.textContent = button;  // When number is typed after sign
+    }
+
+}
