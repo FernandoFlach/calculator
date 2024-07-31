3225ff7f7b32730a8d75941c068f5b26166471c8
Fernando Flach <fernandoflach88@gmail.com>
2024-07-31 17:54:10 -0300

    Change buttons from <div> to <button>.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// const button = document.querySelector(".buttonTest");
+// button.addEventListener("click", () => alert("buttonclicked"));
+
+const buttons = [...document.querySelectorAll(".button")];
+
+const signs = [...document.querySelectorAll(".button")].filter((id) => id.id.includes("Sign"));
+
