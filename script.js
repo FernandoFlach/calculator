65d82490822c6e7ae6f0a4733cce4678fe8f685d
Fernando Flach <fernandoflach88@gmail.com>
2024-08-01 14:52:09 -0300

    Finished calculator operator and number functions.

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 
-let firstNumber, operator, secondNumber;
+var firstNumber, operator, secondNumber;
 
+var equalSignPressed;
+let lastButtonPressed;
 
 const display = document.querySelector("#display")
 
 const numbers = [...document.querySelectorAll(".button")].filter((id) => id.id.includes("b"));  // Includes the operation signs
 const signs = [...document.querySelectorAll(".button")].filter((id) => id.id.includes("Sign")); // Clear, backspace, dot, equal sign
 
+// Creates eventlisteners for all the buttons
 for (i in numbers){
     let number = numbers[i].id[1];
     numbers[i].addEventListener("click", () => {
-        alterScreen(number);
+       // alterScreen(number);
+        changeDisplay(number);
+        console.log(firstNumber)
+        console.log(operator)
+        console.log(secondNumber)
+        console.log(equalSignPressed)
     })
 }
 
+for (i in signs){
+    let id = signs[i].id;
+    if (id == "equalSign"){
+        signs[i].addEventListener("click", () => {
+            changeDisplay("=")
+            console.log("operator", operator)
+
+        })
+    }
+    else if (id == "clearSign"){  // Clear (A/C)
+        signs[i].addEventListener("click", () => {
+            display.textContent = 0;
+            firstNumber = undefined;
+            secondNumber = undefined;
+            operator = undefined;
+
+    })}
+    else if (id == "deleteSign"){  // Backspace
+        signs[i].addEventListener("click", () => {
+            display.textContent = Math.floor(display.textContent / 10);
+        })
+    }
+}
 
 
 function add(a, b){
@@ -29,35 +60,77 @@ function multiply(a,b) {
 }
 
 function divide(a,b){
-    return a / b;
+    return  Math.round(100 * (a / b)) / 100; // Rounds to 2 decimal places
 };
-
 
 function operate(a,b,op){
     if (op == "+"){
-        add(a,b);
+        return add(a,b);
     } 
     else if (op == "-"){
-        subtract(a,b)
+        return subtract(a,b)
     }
     else if (op == "/"){
-        divide(a,b);
+        return divide(a,b);
     }
     else if (op == "x"){
-        multiply(a,b);
+        return multiply(a,b);
     }
 }
 
-function alterScreen(button){
 
-    if (isNaN(button)){  
-        display.textContent = button;  // For operation signs
-    }
-    else if (!isNaN(display.textContent)){  // When number has more than one digit
-        display.textContent += button;
-    }
-    else {
-        display.textContent = button;  // When number is typed after sign
-    }
+function changeDisplay(input){
+    
 
+
+    operatorFunction(input);
+    equalSignFunction(input);
+    numberFunction(input, lastButtonPressed);
+
+    lastButtonPressed = input;
+
+
+
+function operatorFunction(input){
+    if ("/x-+".includes(input)){
+        if (operator == undefined){            // First time pressed
+            firstNumber = display.textContent;
+            operator = input; 
+        }
+        else {
+            secondNumber = display.textContent;
+            firstNumber = operate(firstNumber,secondNumber,operator);
+            display.textContent = firstNumber;
+            operator = input;
+
+        }
+    }
 }
+
+function equalSignFunction(input){
+    if (input == "="){  
+        secondNumber = display.textContent;
+        firstNumber = operate(firstNumber,secondNumber,operator);
+        display.textContent = firstNumber;
+        operator = undefined;
+        equalSignPressed = true;
+    }
+}
+
+function numberFunction(input,lastInput){
+
+    // If input is a number
+    if (!isNaN(input)){  
+        // For default 0 displayed
+        if (display.textContent == 0){
+            display.textContent = input;
+        }
+        else if ("+-/x".includes(lastInput)){
+            display.textContent = input;
+        }
+        else {
+            display.textContent += input;
+        }
+    }
+
+}}
